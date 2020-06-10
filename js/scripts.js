f2a379f718e9b0392370614d17d157d468bf3603
Chris Yoon <chy.yoon@gmail.com>
2020-06-10 11:27:33 -0700

    Adds a function that moves the first char to the end of the word
    co-authored-by: Erich Richter<erich.richter@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,31 @@
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    const num1 = parseInt($("input#num1").val());
-    const num2 = parseInt($("input#num2").val());
     
-    let elements = num1 / num2; 
-    let array=[];
-
-    if(num1, num2) {
-      if (num2 > num1) {
-        alert("Please enter a larger 'count to' number");
-      }
-      else {
-        if ((num1 <= 0) || (num2 <= 0)) {
-          alert("Please enter a larger number")
+    const vowels = ['a', 'e','i','o', 'u'];
+    const consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y'];
+    const userWord = "apple";
+    pigLatin(userWord);
+    function pigLatin (userWord) {  //checks to see if first char is vowel
+      let newArray = userWord.split("");
+      let outputArray = newArray.slice();
+      let output;
+      
+      vowels.forEach(function(vowel){  //if first char is a vowel, removes the vowel and adds the vowel to the end of the word
+        if (newArray[0]===vowel) {
+          outputArray.push(outputArray.shift());
+          outputArray.push("w","a","y");
+          output = outputArray.join("");
         }
-        else {
-          if (isNaN(num1,num2)) {
-            alert("Please enter a number");  
-          }
-          else {
-            for(let i=0; i<elements; i++) {
-            array.push(i * num2);
-            }
-            stringArray = array.join(", ");
+      });
+      console.log(output);
+      return output;
 
-            $(".num1").append(num1);
-            $(".num2").append(num2);
-            $(".array").append(stringArray);
-            $("#output").show();
-          }
+      consonants.forEach(function(consonant) {
+        if (newArray[0]===consonant) {
+          for (i =)
         }
-      }
-  }
-  else {
-    alert ("enter a number you rascal")
-  }
+      });
+    }
   });
 });
-
-/*if (num1 > num2) {
-  alert("'count to' must be larger");
-} else if (!num1 || !num2) {
-  alert("Please enter a number");
-} else {
-  for(let i=0; i<elements; i++) {
-    array.push(i * num2);
-  }*/
