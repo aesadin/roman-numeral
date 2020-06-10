008d48f0cbccdd90af24d27893e2ecf3b6a244d8
Chris Yoon <chy.yoon@gmail.com>
2020-06-10 15:58:31 -0700

    Seperate business and User interface logic
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-Authored-by: Erich Richter<erich.richter@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,12 @@
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    
-    const sentence = $("input#sentence").val();
-    
-    const vowels = ["a", "e", "i", "o", 'u'];   
-    let wordsArray = sentence.split(" ");
-    
-    
+    let sentence;
+    $("input#sentence").each(function(){
+      sentence = $(this).val();
+    });  
     const regex = /^[0-9]+$/i
+    let wordsArray = sentence.split(" ");
 
     if (regex.test(sentence)) {
         alert("Use your Words");
@@ -17,26 +15,27 @@ $(document).ready(function() {
     else {
       $("#show-me").append("<li>" + wordsArray.map(word => pigLatin(word)).join(" ") + "</li>");
     }
-    
-
-    function pigLatin (inputWord) {  
-      let inputArray = inputWord.split(""); 
-      let output;
-      for (i = 0; i < inputArray.length; i++) {
-        if (vowels.indexOf(inputArray[i]) !== -1) {  //breaks if there is a vowel 
-          break;
-          }
-        if (inputArray.slice(i, i+2).join("") === "qu") { //
-          i++;
-        	}
-        }
-        if (i === 0) {
-        output = inputArray.join("") + "way"   // fixes apple
-        }
-        else {
-        output = inputArray.slice(i).join("") + inputArray.slice(0, i).join("") + "ay"  //
-        }
-        return output;
-        }
   });
+
 });
+
+  //Business logic
+  function pigLatin (inputWord) {  
+    const vowels = ["a", "e", "i", "o", 'u'];  
+    let inputArray = inputWord.split(""); 
+    let output;
+    for (i = 0; i < inputArray.length; i++) {
+      if (vowels.indexOf(inputArray[i]) !== -1) {  //breaks if there is a vowel. 
+        break;
+        }
+      if (inputArray.slice(i, i+2).join("") === "qu") { //determines if it two consecutive chars === qu.
+        i++;
+        }
+    }
+    if (i === 0) {
+    output = inputArray.join("") + "way"   // fixes apple
+    } else {
+    output = inputArray.slice(i).join("") + inputArray.slice(0, i).join("") + "ay"  //
+    }
+    return output;
+  }
